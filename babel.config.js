87e9cdffdc8410158075b7cbf3d01567ea440999
aaronbruneau <plymthman123@yahoo.com>
2026-04-30 14:10:17 -0400

    Fix @/ path alias resolution for Vercel builds via babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,12 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      ['module-resolver', {
+        root: ['./'],
+        alias: { '@': './' },
+        extensions: ['.ios.js', '.android.js', '.js', '.jsx', '.ts', '.tsx', '.json'],
+      }],
+    ],
   };
 };
